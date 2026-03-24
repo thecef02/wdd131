91868a7606117f0adb1051668ecafdc870706310
thecef02 <thecef@hotmail.com>
2026-03-24 14:14:29 -0600

    03-24-26

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,7 @@
+let yr = new Date().getFullYear();
+document.getElementById("currentyear").textContent =
+  `${yr} Cristian Fernandez | Utah, USA`;
+
+const d = new Date(document.lastModified);
+document.getElementById("lastModified").textContent =
+  `Last Modify ${d.toLocaleString("en-US", { hour12: false }).replace(",", "")}`;
